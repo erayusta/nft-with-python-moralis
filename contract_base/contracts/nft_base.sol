ecdecafe04dacc144960594f913b50b004be3ce3
erayusta <eray2509@gmail.com>
2022-02-22 21:12:42 +0300

    contract edit

--- contract_base/contracts/nft_base.sol
+++ contract_base/contracts/nft_base.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "OpenZeppelin/openzeppelin-contracts@4.0.0/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "OpenZeppelin/openzeppelin-contracts@4.0.0/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract MyNFT is ERC721URIStorage {
+contract NilNFT is ERC721URIStorage {
     using Counters for Counters.Counter;
     
     Counters.Counter private _tokenIds;
